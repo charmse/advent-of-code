5aecbda9e97c627c315ac70e9d004c7bc4200181
CH073217 <cale.harms@cerner.com>
2020-12-02 00:14:10 -0600

    2020, Day 1: Add part 2

--- 2020/day01/solution.py
+++ 2020/day01/solution.py
@@ -1,37 +1,64 @@
 """ Day 1: Report Repair """
 
 import time
+from absl import app
+from absl import flags
 
+FLAGS = flags.FLAGS
 
-def find_pair(numbers):
+flags.DEFINE_integer('sum', 2020, 'sum of pairs in list to find solution for')
+
+def part_one(numbers):
     """ dumb function to find pair that sum to 2020 """
     for i in numbers:
-        difference = 2020 - i
-        if difference in numbers:
-            return (i, difference)
+        if FLAGS.sum - i in numbers:
+            return (i, FLAGS.sum - i)
     return (0, 0)
 
 
-def main():
-    """ fetch input, time and call find_pair and print results"""
+
+def part_two(numbers):
+    """ dumb function to find three numbers that sum to 2020 """
+    for i in numbers:
+        j_start = 1
+        for j in numbers[j_start:]:
+            if FLAGS.sum - i - j in numbers[j_start + 1:]:
+                return (i, j, FLAGS.sum - i - j)
+        j_start += 1
+    return (0, 0, 0)
+
+
+def main(argv):
+    """ fetch input, time and call both parts and print results"""
+
     # Read Input
     with open('./in.txt', 'r', encoding='utf-8') as file:
         numbers = list(map(int, file.readlines()))
-    print(f'Input: {input}\n')
+    print(f'Input: {numbers}\n')
 
-    # Find pair
+    # part 1
     print('Finding pair...')
     begin = time.time()
-    pair = find_pair(numbers)
+    pair = part_one(numbers)
     time.sleep(1)
-    time_taken = time.time() - begin
+    time_taken_1 = time.time() - begin
 
     # Print results and solution
-    print(f'Pair: {numbers}')
-    print(f'Solution: {pair[0]*pair[1]}')
-    print(f'Time took: {time_taken}')
+    print(f'Part 1 Pair: {pair}')
+    print(f'Part 1 Solution: {pair[0]*pair[1]}')
+    print(f'Time took in part 1: {time_taken_1}\n')
 
+    # part 2
+    print('Finding triple...')
+    begin = time.time()
+    triple = part_two(numbers)
+    time.sleep(1)
+    time_taken_2 = time.time() - begin
+
+    # Print results and solution
+    print(f'Part 2 triple: {triple}')
+    print(f'Part 2 Solution: {triple[0]*triple[1]*triple[2]}')
+    print(f'Time took in part 2: {time_taken_2}\n')
 
 if __name__ == "__main__":
-    main()
-
+    app.run(main)
