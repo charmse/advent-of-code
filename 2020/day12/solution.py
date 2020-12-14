ef322e0b96e767f9acfea63df363ad2ac16e6f9c
Cale Harms <caleharms@protonmail.ch>
2020-12-13 21:39:02 -0600

    2020, Day 12, Part 2

--- 2020/day12/solution.py
+++ 2020/day12/solution.py
@@ -1,12 +1,16 @@
 """ Day 12: Rain Risk """
 
+
 class Ship:
     def __init__(self):
         self.deg_sign = {'R': 1, 'L': -1}
         self.dir = ['N', 'E', 'S', 'W']
         self.facing = {'N': False, 'E': True, 'S': False, 'W': False}
         self.state = {'N': 0, 'E': 0, 'S': 0, 'W': 0}
-        self.curr = lambda : list(self.facing.keys())[list(self.facing.values()).index(True)]
+        self.waypoint = {'N': 1, 'E': 10, 'S': 0, 'W': 0}
+
+    def curr(self):
+        return list(self.facing.keys())[list(self.facing.values()).index(True)]
 
     def move(self, ins):
         if ins[0] == 'R' or ins[0] == 'L':
@@ -14,7 +18,7 @@ class Ship:
         elif ins[0] == 'F':
             self.update(self.curr(), int(ins[1:]))
         else:
-            self.update(ins[0] , int(ins[1:]))
+            self.update(ins[0], int(ins[1:]))
 
     def steer(self, dir, deg):
         curr = self. curr()
@@ -26,6 +30,33 @@ class Ship:
 
     def update(self, dir, units):
         self.state[dir] += units
+
+    def move2(self, ins):
+        if ins[0] == 'R' or ins[0] == 'L':
+            self.rotate_waypt(ins[0], int(ins[1:]))
+        elif ins[0] == 'F':
+            self.move_to_waypt(int(ins[1:]))
+        else:
+            self.update_waypt(ins[0], int(ins[1:]))
+
+    def rotate_waypt(self, dir, deg):
+        times = int(deg/90)
+        for n in range(times):
+            n = self.waypoint['N']
+            for i in range(1, len(self.waypoint)):
+                last = self.waypoint[self.dir[self.deg_sign[dir]*i]]
+                self.waypoint[self.dir[self.deg_sign[dir]*i]] = n
+                n = last
+            self.waypoint['N'] = n
+
+    def update_waypt(self, dir, units):
+        self.waypoint[dir] += units
+
+    def move_to_waypt(self, units):
+        self.state['N'] += units * self.waypoint['N']
+        self.state['E'] += units * self.waypoint['E']
+        self.state['S'] += units * self.waypoint['S']
+        self.state['W'] += units * self.waypoint['W']
 
     def manhattan_dist(self):
         s = self.state
@@ -40,7 +71,10 @@ def part_one(ins):
 
 
 def part_two(ins):
-    return 0
+    ship = Ship()
+    for i in ins:
+        ship.move2(i)
+    return ship.manhattan_dist()
 
 
 def main():
@@ -67,4 +101,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
