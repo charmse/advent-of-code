af9faa484ae820f0c6ab0ad50dc954f01c7007ba
CH073217 <cale.harms@cerner.com>
2020-12-16 21:21:42 -0600

    16-12-2020: Finish part 1

--- 2020/day16/solution.py
+++ 2020/day16/solution.py
@@ -3,21 +3,20 @@
 from collections import defaultdict
 
 
+def invalid(fields, tickets):
+    error = []
+    for ticket in tickets:
+        for t in ticket:
+            valid = [f[2] <= t <= f[3] or f[0] <= t <= f[1] for f in fields]
+            if True not in valid:
+                error.append(t)
+    return error
+
 def solve(fields, tickets):
-    ts = []
-    for t in tickets:
-        valid = True
-        for f, t in zip(fields, t):
-            print(f)
-            print(t)
-            a = f[0] <= t <= f[1] or f[2] <= t <= f[3]
-            print(a)
-            if not(f[0] <= t <= f[1] or f[2] <= t <= f[3]):
-                valid = False
-                break
-        ts.append(valid)
-    print(ts)
+    bad = invalid(fields, tickets[1:])
+    ts = list(filter(lambda t: not(any(b in t for b in bad)), tickets[1:]))
     return 1
+
 
 def parse(l):
     name, vals = l.split(':')
@@ -25,8 +24,10 @@ def parse(l):
     vals = vals[0].strip() + '-' + vals[1].strip()
     return [int(i) for i in vals.strip().split('-')]
 
+
 with open('ex.txt', 'r', encoding='utf-8') as file:
     ls = list(map(str.strip, file.readlines()))
     fields = [parse(l) for l in ls[:ls.index('')]]
     tickets = [list(map(int, l.split(','))) for l in [ls[ls.index('')+2]] + ls[ls.index('')+5:]]
+    print(sum(invalid(fields, tickets[1:])))
     print(solve(fields, tickets))
