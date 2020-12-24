b0d50486ebb5a8bc2a6bf538452e1cb5a0c53c84
CH073217 <cale.harms@cerner.com>
2020-12-23 22:35:34 -0600

    Finish day 16 & 17

--- 2020/day16/solution.py
+++ 2020/day16/solution.py
@@ -1,64 +1,29 @@
-""" Day 16: Ticket Translation """
+import math
+import re
 
-from itertools import permutations
 import numpy as np
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import maximum_bipartite_matching
 
+with open('in.txt') as f:
+    ls = [line.strip() for line in f.readlines()]
 
-def p1(fields, tickets):
-    fs = [vs for n, vs in fields]
-    error = []
-    for ticket in tickets:
-        for t in ticket:
-            valid = [f[2] <= t <= f[3] or f[0] <= t <= f[1] for f in fs]
-            if True not in valid:
-                error.append(t)
-    return error
+ranges = [list(map(int, re.findall('\d+', x))) for x in ls[:20]]
+your = np.array([int(x) for x in ls[22].split(',')], dtype=np.int64)
+nearby = [list(map(int, re.findall('\d+', x))) for x in ls[25:]]
 
+# Part one
+valid = set()
+for t1, t2, t3, t4 in ranges:
+    valid |= set(range(t1, t2 + 1))
+    valid |= set(range(t3, t4 + 1))
 
-def print_dict(d):
-    cols = list(map(list, zip(*list(d.values()))))
-    print(f'\t'.expandtabs(20), end='')
-    for c in cols:
-        print(sum(c), end=' ')
-    print(f'\t\n'.expandtabs(20))
-    for k in d.keys():
-         print(f'{k}:\t'.expandtabs(20), end='')
-         print(*d[k], end='\t\t')
-         print(sum(d[k]))
+print(sum(n for l in nearby for n in l if n not in valid))
 
-
-def p2(fields, tickets):
-    bad = p1(fields, tickets[1:])
-    ts = list(filter(lambda t: not(any(b in t for b in bad)), tickets[1:]))
-    tts = list(map(list, zip(*ts)))
-    names = [n for n, vs in fields]
-    vals = [vs for n, vs in fields]
-    d = dict()
-    for n, v in zip(names, vals):
-        valid = []
-        for c in tts:
-            f = 1
-            for r in c:
-                if not v[0] <= r <= v[1] and not v[2] <= r <= v[3]:
-                    f = 0
-                    break
-            valid.append(f)
-        d[n] = valid
-    print_dict(d)
-    return 1
-
-
-
-def parse(l):
-    name, vals = l.split(':')
-    vals = vals.strip().split('or')
-    vals = vals[0].strip() + '-' + vals[1].strip()
-    return (name, [int(i) for i in vals.strip().split('-')])
-
-
-with open('in.txt', 'r', encoding='utf-8') as file:
-    ls = list(map(str.strip, file.readlines()))
-    fields = [parse(l) for l in ls[:ls.index('')]]
-    tickets = [list(map(int, l.split(','))) for l in [ls[ls.index('')+2]] + ls[ls.index('')+5:]]
-    print(sum(p1(fields, tickets[1:])))
-    print(p2(fields, tickets))
+# Part two
+valids = [l for l in nearby if all(n in valid for n in l)]
+loc = [[all((t1 <= l[j] <= t2) or (t3 <= l[j] <= t4) for l in valids)
+        for t1, t2, t3, t4 in ranges]
+       for j in range(20)]
+m = maximum_bipartite_matching(csr_matrix(loc))
+print(your[m[:6]].prod())
